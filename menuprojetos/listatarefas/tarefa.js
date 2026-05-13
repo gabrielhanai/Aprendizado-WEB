b83250d8c8b4c506aa7397201f9bfb61fb48ebc7
Gabriel Hanai <gabrielhanai1@gmail.com>
2026-05-13 16:52:27 -0300

    edit de elementos da lista

--- menuprojetos/listatarefas/tarefa.js
+++ menuprojetos/listatarefas/tarefa.js
@@ -1,30 +1,44 @@
+let tarefas = []
 function adicionartarefa() {
-    // recebe valor do input do usuario
     let inputtarefa = document.querySelector("#inputtarefa");
     let tarefa = inputtarefa.value.trim();
-
-    // verifica se tem input
     if (tarefa === ""){
         alert("Digite uma tarefa para adicionar à lista!");
     }
+
     else{
         console.log(tarefa);
-
-    // cria um elemento li e adiciona a tarefa do usuario dentro da tag da lista ul
+        tarefas.push(tarefa);
         let listatarefas = document.querySelector("#lista");
         let novatarefa = document.createElement("li");
+        let botaoeditar = document.createElement("button");
+        botaoeditar.id = "botaoeditar";
+        botaoeditar.textContent = "Editar";
         novatarefa.textContent = tarefa;
+        botaoeditar.onclick = function() {
+            let novotexto = prompt("Digite o novo texto para a tarefa:");
+            let novotexto2 = novotexto.trim();
+            if (novotexto2 === ""){
+                alert("Insira um valor válido!")
+            }
+            else{
+                novatarefa.textContent = novotexto2;
+            }
+        }
         listatarefas.appendChild(novatarefa);
-
-    //coloca o input do usuario vazio novamente
+        listatarefas.appendChild(botaoeditar);
         inputtarefa.value = "";
+        console.log(tarefas);
     }
 }
 function apagartarefas(){
     let tarefas = document.querySelectorAll("li");
     tarefas.forEach(tarefa => {
         tarefa.remove();
+        let botaoeditar = document.getElementById("botaoeditar");
+        botaoeditar.remove();
     });
+    tarefas = [];
 }
 
 function mudarTema() {
@@ -38,12 +52,3 @@ function mudarTema() {
         titulo.style.color = "black"
     }
 }
-// MODELO ALTERADO SEM ID #TITULO PARA COPIAR E COLAR NOS OUTROS CODIGOS
-/* function mudarTema() {
-    if (document.body.style.backgroundColor === "white"){
-        document.body.style.backgroundColor = "black"
-    }
-    else{
-        document.body.style.backgroundColor = "white"
-    }
-} */
