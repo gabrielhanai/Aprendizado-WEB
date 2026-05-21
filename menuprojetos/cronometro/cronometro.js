a2abde40b30ba167ebc3ac3404f382e57ce2cd4b
Gabriel Hanai <gabrielhanai1@gmail.com>
2026-05-21 15:03:51 -0300

    modo escuro cronometro

--- menuprojetos/cronometro/cronometro.js
+++ menuprojetos/cronometro/cronometro.js
@@ -1,3 +1,15 @@
+function mudartema(){
+    let body = document.querySelector("body")
+    if (body.style.backgroundColor === "white"){
+        body.style.backgroundColor = "hsl(0, 0%, 5%)"
+        body.style.color = "white"
+    }
+    else{
+        body.style.backgroundColor = "white"
+        body.style.color = "black"
+    }
+}
+
 function iniciar(){
 
 }
