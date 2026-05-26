d6746131bf6bb9940dcd0b79a52a941de4c39431
Gabriel Hanai <gabrielhanai1@gmail.com>
2026-05-26 15:14:12 -0300

    reformulação html conversor

--- menuprojetos/conversor/conversor.js
+++ menuprojetos/conversor/conversor.js
@@ -1,3 +1,6 @@
+let input1 = document.querySelector("#input1")
+let input2 = document.querySelector("#input2")
+
 function mudartema(){
     let body = document.querySelector("body")
     let select = document.querySelectorAll("select")
@@ -6,4 +9,15 @@ function mudartema(){
         input.classList.toggle("escuro")
     });
     // OBS UM JEITO MT BOM PRA FAZER MUDANCA DE TEMAS É USANDO .TOGGLE E INTEGRANDO COM O CSS
+}
+function converter(){
+    let input1value = input1.value
+    if(input1value == ""){
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+    else if (input1value < 0){
+        alert("Por favor, insira um valor válido.");
+        return;
+    }
 }
