d4849224765fa373d34327d80015cefbeb2b4969
Gabriel Hanai <gabrielhanai1@gmail.com>
2026-05-29 18:37:01 -0300

    reestilizacao geradorsenhas

--- menuprojetos/geradorsenhas/geradorsenha.js
+++ menuprojetos/geradorsenhas/geradorsenha.js
@@ -1,7 +1,12 @@
 let body = document.querySelector("body")
 let gerar = document.querySelector("#gerar")
 let display = document.querySelector("#display")
+let caracteres = "!@#$%&*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*"
 function mudartema(){
     body.classList.toggle("escuro")
     gerar.classList.toggle("escuro")
+    display.classList.toggle("escuro")
+}
+function gerarsenha(){
+    
 }
